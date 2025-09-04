9a5eba5017d7db94ec6d57084d7a7d2a616e268d
RudySeo <gun100491@gmail.com>
2025-09-04 16:48:41 +0900

    feat(logger): 공통 로그 적용

--- src/main/kotlin/com/example/board/kafka/BoardKafkaConsumer.kt
+++ src/main/kotlin/com/example/board/kafka/BoardKafkaConsumer.kt
@@ -1,16 +1,20 @@
 package com.example.board.kafka
 
+import com.example.board.Board
+import com.example.board.BoardRepository
 import com.example.board.kafka.dto.TestDto
-import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Service
 
 @Service
-class BoardKafkaConsumer {
-    private val logger = LoggerFactory.getLogger(BoardKafkaConsumer::class.java)
+class BoardKafkaConsumer(private val boardRepository: BoardRepository) : Loggable() {
 
     @KafkaListener(topics = ["board-topic3"])
     fun consume(dto: TestDto) {
         logger.info("Kafka 메시지 수신: $dto")
+        val saver = Board(title = dto.title, content = dto.content)
+        boardRepository.save(saver)
+
+        logger.info("Kafka 메시지 수신완료: $saver")
     }
 }
